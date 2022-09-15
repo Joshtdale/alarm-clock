2324fa818b253793a9b55061a12780330cb69035
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-09-15 19:49:07 +0000

    select style

--- js/main.js
+++ js/main.js
@@ -3,22 +3,24 @@ const alarmBtn = document.getElementById('alarm')
 
 function options() {
     // selectors[0].innerHTML = selectors[1].innerHTML = selectors[2].innerHTML = "";
-
     selectors[0].innerHTML = `<option value="Hour" selected disabled hidden>Hour</option>`;
+    selectors[1].innerHTML = `<option value="Minute" selected disabled hidden>Minute</option>`;
+    selectors[2].innerHTML = `<option value="AM/PM" selected disabled hidden>AM/PM</option>`;
+
     for (let i = 12; i > 0; i--) {
         i = i < 10 ? `0${i}` : i;
         let option = `<option value="${i}">${i}</option>`;
         selectors[0].firstElementChild.insertAdjacentHTML("afterend", option);
     }
     
-    selectors[1].innerHTML = `<option value="Minute" selected disabled hidden>Minute</option>`;
+    
     for (let i = 59; i >= 0; i--) {
         i = i < 10 ? `0${i}` : i;
         let option = `<option value="${i}">${i}</option>`;
         selectors[1].firstElementChild.insertAdjacentHTML("afterend", option);
     }
     
-    selectors[2].innerHTML = `<option value="AM/PM" selected disabled hidden>AM/PM</option>`;
+    
     for (let i = 2; i > 0; i--) {
         let ampm = i == 1 ? "AM" : "PM";
         let option = `<option value="${ampm}">${ampm}</option>`;
@@ -36,24 +38,29 @@ const clock = document.getElementById('clock')
     var min = time.getMinutes()
     var sec = time.getSeconds()
 
-    
 
+    if (hrs >= 12) {
+        hrs = hrs - 12;
+            ampm = "PM";
+    };
+
+// has to go after 12hr conversion
     if (hrs < 10) {
         hrs = '0' + hrs;
     };
+    
     if (min < 10) {
         min = '0' + min;
     };
+
     if (sec < 10) {
         sec = '0' + sec;
     };
 
-    if (hrs >= 12) {
-        hrs = hrs - 12;
-            am_pm = "PM";
-    };
+
+    
     // console.log(hrs, min, sec)
-    clock.innerText = hrs + ':' + min + ':' + sec + ' ' + am_pm;
+    clock.innerText = hrs + ':' + min + ':' + sec + ' ' + ampm;
 
 }
 
