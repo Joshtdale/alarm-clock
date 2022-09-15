8fb08e0a32f18ae7683090f2bd52356698766544
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-09-15 13:29:41 +0000

    git

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 
+function getTime() {
+
+    const time = new Date()
+    const hrs = time.getHours()
+    const min = time.getMinutes()
+    const sec = time.getSeconds()
+    console.log(hrs, min, sec)
+}
+
+setInterval(getTime, 1000)
