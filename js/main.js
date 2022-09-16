6ed937c30384920ceaaada49530ace9c8d02787a
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-09-16 01:07:52 +0000

    stuff

--- js/main.js
+++ js/main.js
@@ -14,18 +14,18 @@ function select() {
     for (let i = 12; i > 0; i--) {
         i = i < 10 ? `0${i}` : i;
         let option = `<option value="${i}">${i}</option>`;
-        selectors[0].firstElementChild.insertAdjacentHTML("afterend", option); 
-    //The insertAdjacentHTML() method of the Element interface parses 
-    //the specified text as HTML or XML and inserts the resulting nodes into the DOM tree at a specified position.
+        selectors[0].firstElementChild.insertAdjacentHTML("afterend", option);
+        //The insertAdjacentHTML() method of the Element interface parses 
+        //the specified text as HTML or XML and inserts the resulting nodes into the DOM tree at a specified position.
     }
-    
+
     // Loop through 59min for selector
     for (let i = 59; i >= 0; i--) {
         i = i < 10 ? `0${i}` : i;
         let option = `<option value="${i}">${i}</option>`;
         selectors[1].firstElementChild.insertAdjacentHTML("afterend", option);
     }
-    
+
     // Loop through 2 options for am/pm
     for (let i = 2; i > 0; i--) {
         let ampm = i == 1 ? "AM" : "PM"; // assigns values 1 & 2 to AM & PM
@@ -38,7 +38,7 @@ select();
 
 
 function getTime() {
-const clock = document.getElementById('clock')
+    const clock = document.getElementById('clock')
     const time = new Date()
     var hrs = time.getHours()
     var min = time.getMinutes()
@@ -46,20 +46,20 @@ const clock = document.getElementById('clock')
 
 
 
-// if hrs is greater than or = to 12 then the time is PM else AM
+    // if hrs is greater than or = to 12 then the time is PM else AM
     if (hrs >= 12) {
         hrs = hrs - 12;
-            ampm = "PM";
+        ampm = "PM";
     } else {
         ampm = "AM";
     };
 
-// has to go after 12hr conversion
-// add 0 before number is number is less than 10 "01, 02, 03"
+    // has to go after 12hr conversion
+    // add 0 before number is number is less than 10 "01, 02, 03"
     if (hrs < 10) {
         hrs = '0' + hrs;
     };
-    
+
     if (min < 10) {
         min = '0' + min;
     };
@@ -68,16 +68,18 @@ const clock = document.getElementById('clock')
         sec = '0' + sec;
     };
 
-// clock text = values of hrs, min, sec, and ampm
-// adds number values together with colon ":" and ' (single space)' so that innerText = hrs:min:sec am/pm
+    // clock text = values of hrs, min, sec, and ampm
+    // adds number values together with colon ":" and ' (single space)' so that innerText = hrs:min:sec am/pm
     clock.innerText = hrs + ':' + min + ':' + sec + ' ' + ampm;
     // clock.innerText = `${hrs}:${min}:${sec} ${ampm}`;
-const alarmTime = selectors[0].value + ':' + selectors[1].value + ':' + '00' +' ' + selectors[2].value;
-        if (alarmTime === clock.innerText) {
-    console.log('alarm working')
-    alert('working')
-        };
-        console.log(alarmTime)
+    const alarmTime = selectors[0].value + ':' + selectors[1].value + ':' + '00' + ' ' + selectors[2].value;
+    if (alarmTime === clock.innerText) {
+
+       
+        console.log('alarm working')
+        alert('working')
+
+    };
 };
 
 // updates time value from getTime function every 1000 milliseconds so that clock counts seconds in real time
@@ -88,5 +90,9 @@ setInterval(getTime, 1000)
 alarmBtn.addEventListener('click', setAlarm);
 
 function setAlarm() {
-    console.log('clicked')
+    document.getElementById('myForm').reset()
 }
+
+const img = document.createElement('img')
+img.setAttribute('src', '/images/wake-up.jpeg') 
+document.body.appendChild('img')
