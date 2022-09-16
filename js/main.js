7f5498f0f63721e41c6d93d9545f8f9bde20c0a1
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-09-16 20:22:36 +0000

    ampm fix

--- js/main.js
+++ js/main.js
@@ -11,28 +11,29 @@ function select() {
     selectors[2].innerHTML = `<option value="AM/PM" selected disabled hidden>AM/PM</option>`;
 
     // Loop through 12hrs for selector
-    for (let i = 12; i > 0; i--) {
-        i = i < 10 ? `0${i}` : i;
-        let option = `<option value="${i}">${i}</option>`;
-        selectors[0].firstElementChild.insertAdjacentHTML("afterend", option);
-        //The insertAdjacentHTML() method of the Element interface parses 
-        //the specified text as HTML or XML and inserts the resulting nodes into the DOM tree at a specified position.
-    }
+        for (let i = 12; i > 0; i--) {
+            i = i < 10 ? `0${i}` : i;
+            let option = `<option value="${i}">${i}</option>`;
+            selectors[0].firstElementChild.insertAdjacentHTML("afterend", option);
+            //The insertAdjacentHTML() method of the Element interface parses 
+            //the specified text as HTML or XML and inserts the resulting nodes into the DOM tree at a specified position.
+        }
 
-    // Loop through 59min for selector
-    for (let i = 59; i >= 0; i--) {
-        i = i < 10 ? `0${i}` : i;
-        let option = `<option value="${i}">${i}</option>`;
-        selectors[1].firstElementChild.insertAdjacentHTML("afterend", option);
-    }
+        // Loop through 59min for selector
+        for (let i = 59; i >= 0; i--) {
+            i = i < 10 ? `0${i}` : i;
+            let option = `<option value="${i}">${i}</option>`;
+            selectors[1].firstElementChild.insertAdjacentHTML("afterend", option);
+        }
 
-    // Loop through 2 options for am/pm
-    for (let i = 2; i > 0; i--) {
-        let ampm = i == 1 ? "AM" : "PM"; // assigns values 1 & 2 to AM & PM
-        let option = `<option value="${ampm}">${ampm}</option>`;
-        selectors[2].firstElementChild.insertAdjacentHTML("afterend", option);
-    }
+        // Loop through 2 options for am/pm
+        for (let i = 2; i > 0; i--) {
+            let ampm = i == 1 ? "AM" : "PM"; // assigns values 1 & 2 to AM & PM
+            let option = `<option value="${ampm}">${ampm}</option>`;
+            selectors[2].firstElementChild.insertAdjacentHTML("afterend", option);
+        }
 }
+
 select();
 
 
@@ -43,43 +44,46 @@ function getTime() {
     var hrs = time.getHours()
     var min = time.getMinutes()
     var sec = time.getSeconds()
-
+    var ampm = 'PM';
     // if hrs is greater than or = to 12 then the time is PM else AM
-    if (hrs > 12) {
-        hrs = hrs - 12;
-    } else if (hrs >=12 ) {
-        ampm = "PM";
-    } else {
-        ampm = "AM";
-    };
+        if (hrs > 12) {
+            hrs = hrs - 12;
+        } else if (hrs >=12 ) {
+            ampm = "PM";
+        } else {
+            ampm = "AM";
+        };
 
-    // has to go after 12hr conversion
-    // add 0 before number is number is less than 10 "01, 02, 03"
-    if (hrs < 10) {
-        hrs = '0' + hrs;
-    };
+        // has to go after 12hr conversion
+        // add 0 before number is number is less than 10 "01, 02, 03"
+        if (hrs < 10) {
+            hrs = '0' + hrs;
+        };
 
-    if (min < 10) {
-        min = '0' + min;
-    };
+        if (min < 10) {
+            min = '0' + min;
+        };
 
-    if (sec < 10) {
-        sec = '0' + sec;
-    };
+        if (sec < 10) {
+            sec = '0' + sec;
+        };
 
     // adds number values together with colon ":" and ' (single space)' so that innerText = hrs:min:sec am/pm
     clock.innerText = hrs + ':' + min + ':' + sec + ' ' + ampm;
 
     const alarmTime = selectors[0].value + ':' + selectors[1].value + ':' + '00' + ' ' + selectors[2].value;
 
-    if (alarmTime === clock.innerText) {
-        var alarm = new Audio('images/Daydreaming.mp3')
-        alarm.play();
-        let clock = document.getElementById('clock')
-        document.body.style.backgroundColor = 'white';
-        clock.style.color = 'black'; 
-    };
+        if (alarmTime === clock.innerText) {
+            var alarm = new Audio('images/Daydreaming.mp3')
+            alarm.play();
+            let clock = document.getElementById('clock')
+            document.body.style.backgroundColor = 'white';
+            clock.style.color = 'black'; 
+        };
 };
+
+
+
 
 // updates time value from getTime function every 1000 milliseconds so that clock counts seconds in real time
 setInterval(getTime, 1000)
@@ -91,6 +95,34 @@ function resetAlarm() {
     document.getElementById('myForm').reset()
 }
 
-// const img = document.createElement('img')
-// img.setAttribute('src', '/images/wake-up.jpeg') 
-// document.body.appendChild('img')
+// Pseudocode
+
+// Alarm clock
+
+
+// time() {
+
+// Get current time from browser: hrs, min, sec
+
+
+// If time.hrs >= (‘greater than or equal to”) 12 {
+// Time is pm
+// } else {
+// Time is Am
+// }
+
+
+// If time.hrs > (‘greater than’) 12 {
+// Hrs = hrs - 12
+// }
+
+
+
+// }
+
+
+// setInterval(time) {
+// loop every 1000 milliseconds
+// }
+
+//new
